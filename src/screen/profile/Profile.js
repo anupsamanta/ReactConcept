02922de986660b3c5c7e0f872c6e60dc2c4d2b75
Anup <anup.iosdeveloper@gmail.com>
2023-06-03 23:10:32 +0530

    Profile_change

--- src/screen/profile/Profile.js
+++ src/screen/profile/Profile.js
@@ -7,37 +7,12 @@ function Profile(props) {
   gobackScreen = ()=>{
     props.navigation.goBack()
   }
-  gotoConceptScreen = () =>{
-    props.navigation.navigate("Concept")
-    
-  }
-  gotoApiScreen = () =>{
-    props.navigation.navigate("CounterScreen")
-  }
-
-  gotoApiScreenNew = () =>{
-    props.navigation.navigate("CounterScreenNew")
-  }
- 
     return (
     <View style = {{flex : 1, alignItems : 'center', justifyContent : 'center'}}>
           <Text style = {{marginBottom : 100, fontSize : 24, color : 'orange', 
           fontWeight : 'bold'}}>Profile Screen</Text>
         <TouchableOpacity onPress = {gobackScreen}>
-          <Text>Back</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress = {gotoConceptScreen}>
-          <Text>GO TO CONCEPT SCREEN</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity onPress = {gotoApiScreen}>
-          <Text>GO TO API CALL</Text>
-        </TouchableOpacity>
-
-
-        <TouchableOpacity onPress = {gotoApiScreenNew}>
-          <Text>GO TO API CALL NEW</Text>
+          <Text>Back to Home screen</Text>
         </TouchableOpacity>
     </View>);
     }
